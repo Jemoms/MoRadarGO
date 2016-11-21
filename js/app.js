6fc6ef4ee6e5c9df51ad3849e86cd90ff2d1d51c
Morej <morej03@gmail.com>
2016-11-21 18:51:40 +0000

    circle ok

--- js/app.js
+++ js/app.js
@@ -1,7 +1,58 @@
         var map;
+        var verdun = {lat: 49.1588, lng: 5.38733 };
         function initMap() {
-          map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: 49.1588, lng: 5.38733 },
-            zoom: 15
-          });
+            var mapOptions = {
+            zoom:17,
+            center: verdun, 
+            disableDefaultUI: true,
+            zoomControl: true
+            }
+            var map = new google.maps.Map(document.getElementById("map"),
+               mapOptions);
+               
+            /////////////START GEOLOCALISATION////// 
+             if (navigator.geolocation && false) {
+                 navigator.geolocation.getCurrentPosition(function (position) {
+                 var pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                 };
+                map.setCenter(pos);
+                });
+
+            }
+            ///////////// END GEOLOCALISATION////// 
+            
+            ///////////// START MARQUEUR POSITION INITIALE////// 
+            var marker = new google.maps.Marker({
+                position: verdun,
+                map: map,
+                draggable: true
+            });
+            ///////////// END MARQUEUR POSITION INITIALE////// 
+            var zeCircle = new google.maps.Circle({
+              strokeColor: '#FF0000',
+              strokeOpacity: 0.8,
+              strokeWeight: 2,
+              fillColor: '#FF0000',
+              fillOpacity: 0.35,
+              map: map,
+              center: verdun,
+              radius: 200
+            });
         }
+        
+        function detectBrowser() {
+          var useragent = navigator.userAgent;
+          var mapdiv = document.getElementById("map");
+        
+          if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1 ) {
+            mapdiv.style.width = '100%';
+            mapdiv.style.height = '100%';
+          } else {
+            mapdiv.style.width = '600px';
+            mapdiv.style.height = '800px';
+          }
+        }
+
+    detectBrowser();
