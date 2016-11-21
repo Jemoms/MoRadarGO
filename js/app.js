5035318b54b9594f55d16e01c16c39a0167b1682
Morej <morej03@gmail.com>
2016-11-21 15:34:50 +0000

    map init verdun center

---
+++ js/app.js
@@ -0,0 +1,7 @@
+        var map;
+        function initMap() {
+          map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 49.1588, lng: 5.38733 },
+            zoom: 15
+          });
+        }
